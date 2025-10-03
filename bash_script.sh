ae7f19f6fb50054d5efaa2240707e203385ef223
Jessica Souza <jessica.souza0408@usp.br>
2025-10-02 21:49:55 -0300

    Solving exercise 1

---
+++ bash_script.sh
@@ -0,0 +1,12 @@
+#P1 - Exercício 2
+
+#O conteúdo desse script contempla:
+
+#Um comando que mostre a pasta/diretório onde você está:
+echo "Diretório atual: $(pwd)"
+
+#Um comando que gere uma uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
+ls > lista_HOME.txt
+
+#Um comando que imprima a data atual na tela:
+echo "Data e hora atuais: $(date)"
