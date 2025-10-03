daab54a2f2097f919ac06ab2b881a29e7a00d67b
Jessica Souza <jessica.souza0408@usp.br>
2025-10-03 12:37:08 -0300

    Solving exercise 2

--- script_area.py
+++ script_area.py
@@ -2,15 +2,31 @@
 
 # Questão 3
 
-import sys
-
-catetoA = int(sys.argv[1])
-catetoB = int(sys.argv[2])
-
-if isdigit(catetoA)==False or catetoA>=500:
-	print("Valor inválido digitado para o cateto A")
+import sys  #Importando o módulo necessário para ler argumentos passados pelo terminal
 
 
-area = (catetoA*catetoB)/2
+#Recebendo os valores dos catetos a partir da linha de comando (fornecidos pelo usuário)
+catetoA = sys.argv[1]
+catetoB = sys.argv[2]
 
-print("A área do triângulo retângulo com lados a={} e b={}, é {}".format(catetoA, catetoB, area))
+
+#Definindo as condições de valores inválidos: não inteiros ou maiores/iguais a 500.
+A_invalido = catetoA.isdigit()==False or int(catetoA)>=500  #Define a invalidade para o valor do cateto A.
+B_invalido = catetoB.isdigit()==False or int(catetoB)>=500  #Define a invalidade para o valor do cateto B.
+#Resolvi adicionar as regras de invalidade como variáveis pois o bloco de códigos a seguir estava ficando mais extenso do que
+#o necessário, visto que para cada condição eu escrevia novamente as condições.
+
+
+#Verifica quais valores são inválidos e imprime mensagens apropriadas
+if A_invalido and B_invalido:  #Ambos os valores são inválidos
+	print("Valores inválidos digitados para ambos os catetos. \nPor gentileza, digite números inteiros menores que 500.")
+#Em um primeiro momento, eu havia colocado esse primeiro bloco "if" como o último "elif", porém seria um bloco inútil, visto
+#que nunca as outras condições já seriam verdadeiras antes dessa chegar.
+elif A_invalido:  #Apenas o valor do cateto A é inválido
+	print("Valor inválido digitado para o cateto A.\nPor gentileza, digite um número inteiro menor que 500.")
+elif B_invalido:  #Apenas o valor do cateto B é inválido
+	print("Valor inválido digitado para o cateto B. \nPor gentileza, digite um número inteiro menor que 500.")
+else:
+	#Caso ambos os valores sejam válidos, a área é calculada e mostrada ao usuário
+	area = (int(catetoA)*int(catetoB))/2  #Calcula a área do triângulo. Nota-se a necessidade de transformar a string do cateto em um inteiro para o cálculo.
+	print("A área do triângulo retângulo com lados a={} e b={}, é {}".format(catetoA, catetoB, area))
