aacf1d34e7fd541af3efba7ab15483ed2cec6b20
Jessica Souza <jessica.souza0408@usp.br>
2025-10-15 13:07:38 -0300

    Adjusting some details

--- bash_script.sh
+++ bash_script.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
+
 #Exercício 2
 
 #O conteúdo desse script contempla:
 
 #Um comando que mostre a pasta/diretório onde você está:
-echo "Diretório atual: $(pwd)"
+echo "Diretório atual: $(pwd)"  #O comando 'pwd' mostra o diretório atual
+#O comando 'echo' foi utilizado para deixar a saída mais organizada e legível, sendo uma escolha mais didática do que simplesmente executar 'pwd' isoladamente.
 
-#Um comando que gere uma uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
-ls > lista_HOME.txt
+
+#Um comando que gere uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
+ls -l ~ > lista_HOME.txt  # O comando 'ls' lista o conteúdo de um diretório; a opção '-l' exibe informações detalhadas; e '~' representa a pasta HOME.
+#A parte '> lista_HOME.txt' redireciona a saída do comando para o arquivo especificado, criando ou sobrescrevendo o arquivo 'lista_HOME.txt' com o resultado da listagem.
+
 
 #Um comando que imprima a data atual na tela:
-echo "Data e hora atuais: $(date)"
+echo "Data e hora atuais: $(date)" #O comando 'date' imprime a data e hora atuais do sistema.
+#Novamente, o 'echo' foi utilizado para formatar melhor a saída, tornando-a mais clara e informativa do que o uso isolado de 'date'.
